ee568324661e7f5d2677bf7cd11f519d81aa0f24
张攀攀 <1432468608@qq.com>
2025-01-03 22:02:01 +0800

    feat: update

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,1 +1,13 @@
-fsdfsdfsd
+import React, {Component} from 'react';
+
+class Index extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Index;
